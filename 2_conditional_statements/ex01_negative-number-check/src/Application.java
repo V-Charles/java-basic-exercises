704125dcae9c5bef819df979610a3e08e96a8420
Vinicius Charles <viniciuscharles3101@gmail.com>
2025-11-25 12:23:55 -0300

    Add number sign verification exercise
    Implement a Java program that reads an integer and checks whether it is negative or not, printing the appropriate message. Includes basic input handling with Scanner and a simple conditional statement.

---
+++ 2_conditional_statements/ex01_negative-number-check/src/Application.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Not negative");
+        }
+        sc.close();
+    }
+}
